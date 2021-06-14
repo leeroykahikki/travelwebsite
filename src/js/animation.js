3e75e913314dd82ae419bf08d1c72d154c27af42
leeroykahikki <60253601+leeroykahikki@users.noreply.github.com>
2021-06-15 02:18:58 +0300

    Some changes

---
+++ src/js/animation.js
@@ -0,0 +1,11 @@
+import anime from './anime.min';
+
+// anime({
+//   targets: '.header-title-img path',
+//   strokeDashoffset: [anime.setDashoffset, 0],
+//   easing: 'easeInOutSine',
+//   duration: 1500,
+//   delay: function(el, i) { return i * 250 },
+//   direction: 'alternate',
+//   loop: true
+// });
