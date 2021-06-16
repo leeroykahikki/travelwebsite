9679622c9145aa9c65e4d108248e96e09dbb86d2
leeroykahikki <60253601+leeroykahikki@users.noreply.github.com>
2021-06-16 12:50:33 +0300

    Cards hover animation

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,11 +1,27 @@
-import anime from './anime.min';
+//! Написать на Stackoverflow о баге с анимацией при резком проигрывании
 
-// anime({
-//   targets: '.header-title-img path',
-//   strokeDashoffset: [anime.setDashoffset, 0],
-//   easing: 'easeInOutSine',
-//   duration: 1500,
-//   delay: function(el, i) { return i * 250 },
-//   direction: 'alternate',
-//   loop: true
-// });
+let cards = document.querySelectorAll('.cards-item');
+
+cards.forEach((card) => {
+  card.addEventListener('mouseenter', (event) => {
+    anime.remove(card);
+    
+    anime({
+      targets: card,
+      filter: 'drop-shadow(0 12px 30px rgba(200, 180, 180, 0.2))',
+      background: '#fff',
+      easing: 'easeInOutQuad',
+    });
+  });
+
+  card.addEventListener('mouseleave', (event) => {
+    anime.remove(card);
+
+    anime({
+      targets: card,
+      filter: 'drop-shadow(rgba(0, 0, 0, 0) 0px 0px 0px)',
+      background: '#f8f8f8',
+      easing: 'easeInOutQuad',
+    });
+  });
+});
